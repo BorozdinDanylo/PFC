df65fa97a92f640ec71f8b7a77515abad4268f20
crypto_tyan <cryptotyantrader@gmail.com>
2025-07-01 19:41:25 +0200

    Added cleaning template after closing

--- template.py
+++ template.py
@@ -1,54 +1,53 @@
 import flet as ft
-from blinker import Signal
 import json
 
 
 
 class Template:
-    standart_content = [
-            ft.TextField(label="Name"),
-            ft.TextField(label="P", value="0", width=100, ),
-            ft.TextField(label="F", value="0", width=100),
-            ft.TextField(label="C", value="0", width=100),
-            ft.TextField(label="X", value="1", width=100),
-        ]
-
-
-
     def __init__(self, page: ft.Page):
-        self.dialog = ft.AlertDialog(
-            title=ft.Text("Template"),
-            content=ft.Column(controls=self.standart_content.copy()),
-            actions=[
-                ft.TextButton("Yes", on_click=self.__save),
-                ft.TextButton("No", on_click=self.__close),
-            ],
-            actions_alignment=ft.MainAxisAlignment.END,
-            alignment=ft.alignment.center,
-
-        )
-
         self.page = page
-
-        self._save = Signal("close")
-        self._save.connect(self.__save, "save")
+        self._clean_dialog()
 
 
     def open(self):
         self.page.open(self.dialog)
 
 
-    def __close(self, *_):
-        # self.dialog.content = ft.Column(controls=copy.deepcopy(self.standart_content))
-        # self.dialog.update()
-
+    def _close(self, *_):
         self.page.close(self.dialog)
+        self._clean_dialog()
 
 
-    def __save(self, *_):
+    def _save(self, *_):
         # noinspection PyTypeChecker
         column: ft.Column = self.dialog.content
         with open(f"templates\\{column.controls[0].value}.json", "w") as f:
             json.dump({field.label: field.value for field in column.controls[1:]}, f)
 
-        self.__close()
+        self._close()
+
+
+    @property
+    def _dialog(self):
+        return ft.AlertDialog(
+            title=ft.Text("Template"),
+            content=ft.Column(
+                controls=[
+                    ft.TextField(label="Name"),
+                    ft.TextField(label="P", value="0", width=100, ),
+                    ft.TextField(label="F", value="0", width=100),
+                    ft.TextField(label="C", value="0", width=100),
+                    ft.TextField(label="X", value="1", width=100),
+                ]
+            ),
+            actions=[
+                ft.TextButton("Yes", on_click=self._save),
+                ft.TextButton("No", on_click=self._close),
+            ],
+            actions_alignment=ft.MainAxisAlignment.END,
+            alignment=ft.alignment.center,
+            # on_dismiss=self._clean_dialog,
+        )
+
+    def _clean_dialog(self, *_):
+        self.dialog = self._dialog
